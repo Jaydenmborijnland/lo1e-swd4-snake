59b3946dcf7b1273ddf298567c41e5e3b700a910
Jaydenmborijnland <6019486@mborijnland.nl>
2022-09-06 14:25:19 +0200

    viekant
    virkant gemaakt

---
+++ snake.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById("snake");
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = 'black';
+ctx.fillRect(0, 0, 400, 400);
+ctx.fillStyle = 'green';
+ctx.fillRect(175, 175, 20, 20);
